0906c2962661f1f26ec5d2c71aa3075603c84b59
ansonkongnyu <118388265+ansonkongnyu@users.noreply.github.com>
2023-04-07 14:30:27 -0400

    Create Boilor plate.cpp

---
+++ Boilor plate.cpp
@@ -0,0 +1,49 @@
+#include <bits\stdc++.h>
+#include<vector> 
+ 
+#define pb push_back
+#define int long long
+#define s string
+#define v vector<int>
+#define fi first
+#define se second
+#define en endl
+#define s string
+#define MOD 1000000007
+#define sl str.length()
+#define SIZE 1e6+2
+ 
+using namespace std;
+int n=0,sum=0;
+ 
+bool isPrime(int num)
+{
+    for (int i = 2; i <= num / 2; ++i) {
+      if (num % i == 0) {
+        return true;
+      }
+    }
+    return false;
+}
+ 
+void solve()
+{
+  cin>>n;
+  int arr[n-1];
+  int ans[n];
+
+ 
+}
+ 
+ 
+ 
+signed main()
+{
+    int tc;
+    cin>>tc;
+    while(tc--)
+    {
+      solve();         
+    } 
+}
+ 
